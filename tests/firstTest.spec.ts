960dc8a765ce23e942dcbae9afdfdc2f00f31229
Lidiia R <lidiia12rusakova@gmail.com>
2025-01-31 13:49:33 +0100

    Add test for Parent elements location

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,9 +1,9 @@
 import {test} from '@playwright/test'
 
 test.beforeEach(async({page}) => {
-    await page.goto("http://localhost:4200")
-    await page.getByText("Forms").click()
-    await page.getByText("Form Layouts").click()
+    await page.goto("http://localhost:4200");
+    await page.getByText("Forms").click();
+    await page.getByText("Form Layouts").click();
 })
 
 test ('User facing locators', async ({page}) => {
@@ -25,5 +25,13 @@ test ('Locating child elements', async ({page}) => {
     await page.locator('nb-card').locator('nb-radio').locator(':text-is("Option 2")').click();
     await page.locator('nb-card').getByRole('button', {name: "Sign in"}).first().click();
     await page.locator('nb-card').nth(3).getByRole('button').click();
+})
 
+test ('Locating parent elements', async ({page}) => {
+    await page.locator('nb-card', {hasText: "Using the Grid"}).getByRole('textbox', {name: "Email"}).click();
+    await page.locator('nb-card', {has: page.locator('#inputEmail')}).getByRole('textbox', {name: "Email"}).click();
+    await page.locator('nb-card').filter({hasText: "Basic form"}).getByRole('textbox',{name: "Email"}).click();
+    await page.locator('nb-card').filter({has: page.locator('.status-danger')}).getByRole('textbox',{name: "Email"}).click();
+    await page.locator('nb-card').filter({has: page.locator('nb-checkbox')}).filter({hasText: "Sign in"}).getByRole('textbox',{name: "Email"}).click();
+    await page.locator(':text-is("Using the Grid")').locator('..').getByRole('textbox',{name: "Email"}).click();
 })
