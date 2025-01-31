b1b2f4d283b994903bec7cd91b842c2d60273f7d
Lidiia R <lidiia12rusakova@gmail.com>
2025-01-31 13:08:16 +0100

    Add test for Child elements lesson

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -7,14 +7,23 @@ test.beforeEach(async({page}) => {
 })
 
 test ('User facing locators', async ({page}) => {
-    await page.getByRole('textbox', {name:"Email"}).first().click()
-    await page.getByRole('button', {name:"Sign in"}).first().click()
+    await page.getByRole('textbox', {name:"Email"}).first().click();
+    await page.getByRole('button', {name:"Sign in"}).first().click();
 
-    await page.getByLabel("Email").first().click()
+    await page.getByLabel("Email").first().click();
 
-    await page.getByPlaceholder('Jane Doe').click()
+    await page.getByPlaceholder('Jane Doe').click();
 
-    await page.getByTestId('SignIn').click()
+    await page.getByTestId('SignIn').click();
 
-    await page.getByText('Using the Grid').click()
+    await page.getByText('Using the Grid').click();
+    await page.getByTitle('IoT Dashboard').click();
+})
+
+test ('Locating child elements', async ({page}) => {
+    await page.locator('nb-card nb-radio :text-is("Option 2")').click();
+    await page.locator('nb-card').locator('nb-radio').locator(':text-is("Option 2")').click();
+    await page.locator('nb-card').getByRole('button', {name: "Sign in"}).first().click();
+    await page.locator('nb-card').nth(3).getByRole('button').click();
+
 })
