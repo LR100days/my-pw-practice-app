fff9657b00d405c0d80375074bd1d481939443f5
Lidiia R <lidiia12rusakova@gmail.com>
2025-02-03 13:56:35 +0100

    Edit the config file for timeouts lesson

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,6 +12,12 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+  timeout: 30000,
+  globalTimeout: 50000,
+
+  // expect: {
+  //   timeout: 3000
+  // },
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -30,6 +36,8 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    //actionTimeout: 5000,
+    //navigationTimeout: 5000,
   },
 
   /* Configure projects for major browsers */
